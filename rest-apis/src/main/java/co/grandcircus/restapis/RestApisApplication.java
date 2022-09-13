0104cc8109d3be03c140cad20d2b458f6c1d9243
Bolanle <bola0321@gmail.com>
2022-09-12 21:15:53 -0400

    start over apis

---
+++ rest-apis/src/main/java/co/grandcircus/restapis/RestApisApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.restapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApisApplication.class, args);
+	}
+
+}
